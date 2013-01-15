c7b195f99bfb0db0100e3a50215c8ccd220958fc
Clint Byrum <clint@fewbar.com>
2013-01-15 00:38:54 -0800

    streamline test with fixtures

--- lib/tests/test_actions.py
+++ lib/tests/test_actions.py
@@ -1,9 +1,9 @@
+import fixtures
 import libvirt
 import mocker
 from mocker import ANY
 from testtools import TestCase
 import tempfile
-import shutil
 import subprocess
 
 from lib import actions
@@ -12,26 +12,19 @@ from lib.args import get_parser
 
 class TestActions(TestCase):
 
-    def setUp(self):
-        super(TestActions, self).setUp()
-        self.parser = get_parser()
-        tdir = tempfile.mkdtemp()
-        self.addCleanup(shutil.rmtree, tdir)
-        self.argv = ['bm_poseur', '--image-path', tdir]
+    def argv(self, cmd):
+        tdir = self.useFixture(fixtures.TempDir()).path
+        return ['bm_poseur', '--image-path', tdir, cmd]
 
     def test_create_vms(self):
-        self.argv.append('create-vm')
+        parser = get_parser()
+        argv = self.argv('create-vm')
         m = mocker.Mocker()
         mock_libv = m.mock()
         mock_call = m.mock()
-        save_open = libvirt.open
-        save_ccall = subprocess.check_call
-        libvirt.open = mock_libv
-        subprocess.check_call = mock_call
-        def _cleanup(sopen, ccall):
-            libvirt.open = sopen
-            subprocess.check_call = ccall
-        self.addCleanup(_cleanup, save_open, save_ccall)
+        self.addCleanup(m.restore)
+        self.useFixture(fixtures.MonkeyPatch('libvirt.open', mock_libv))
+        self.useFixture(fixtures.MonkeyPatch('subprocess.check_call', mock_call))
         mock_libv(settings.QEMU)
         mock_conn = m.mock()
         m.result(mock_conn)
@@ -41,5 +34,4 @@ class TestActions(TestCase):
         mock_call(ANY, shell=ANY)
         mock_call(ANY, shell=ANY)
         m.replay()
-        self.parser.parse_args(self.argv[1:])
-        m.restore()
+        parser.parse_args(argv[1:])
